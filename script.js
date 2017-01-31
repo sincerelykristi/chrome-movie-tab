d21f1c3ca6237a551a1fd7e27594684388b3dfe1
Kristi Murphy <kristilmurphy@gmail.com>
2017-01-31 13:32:22 -0500

    add movie data

---
+++ script.js
@@ -0,0 +1,35 @@
+(function(){
+
+  var images = [
+  "images/squad.jpg",
+  "images/fight.jpg",
+  "images/sunshine",
+  ];
+
+  var quotes = [
+  "'I'm known to be quite vexing. I'm just forewarning you.'",
+  "'We're the middle children of history, man. No purpose or place. We have no Great War. No Great Depression. Our great war is a spiritual war... Our great depression is our lives.'",
+  "'My embarrassing admission is I really like that you're nice, right now.'",
+  ];
+
+  var character = [
+  "- Harley Quinn",
+  "- Tyler Durden",
+  "- Clementine Kruczynski",
+  ];
+
+  var movie = [
+  "Suicide Squad (2016)",
+  "Fight Club (1999)",
+  "Eternal Sunshine (2004)",
+  ];
+
+
+
+
+
+
+
+
+//closing
+})
